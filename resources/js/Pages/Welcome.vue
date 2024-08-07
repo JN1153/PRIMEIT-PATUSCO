27c0c4df7f2a16bf9afe1105d39d2f9fd46c9df8
Joao Neves <joaogilnevess@hotmail.com>
2024-08-07 12:35:08 +0100

    Remove bg from selected Text

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -23,7 +23,7 @@ defineProps({
 <template>
     <Head title="Patusco" />
     <div class="bg-gray-50 text-black ">
-        <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#333333] selection:text-black">
+        <div class="relative min-h-screen flex flex-col items-center justify-center">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                 <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                     <div class="flex lg:justify-center lg:col-start-2">
